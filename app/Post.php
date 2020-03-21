c0f6b172d58e8d5dc94a2fecf6dccbd768d3f3cf
jose perez <joserph.a@gmail.com>
2020-03-20 21:42:56 -0500

    tables and seeder

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'category_id', 'name', 'slug', 'excerpt', 'body', 'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongdTo(User::tag);
+    }
+
+    public function category()
+    {
+        return $this->belongdTo(Category::tag);
+    }
+    
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
